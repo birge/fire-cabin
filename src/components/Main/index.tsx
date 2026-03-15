d08a37d83468f5618d497b3ca13a0c7a6c7e0a08
Birge Clark <birgeclark@gmail.com>
2026-03-14 19:29:11 -0700

    update vite config and readme

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
 import { HOME, LOGIN, PROFILE, REGISTER } from "../../store/page/types";
 import { AppState } from "../../store";
-import Calendar from "../Calendar";
-import Profile from "../Profile";
 import useCurrentUserDetection from "../../hooks/useCurrentUserDetection";
-import LogIn from "../LogIn";
-import Register from "../Register";
+
+const Calendar = React.lazy(() => import("../Calendar"));
+const Profile = React.lazy(() => import("../Profile"));
+const LogIn = React.lazy(() => import("../LogIn"));
+const Register = React.lazy(() => import("../Register"));
 
 const selectPage = (state: AppState) => state.page;
 
@@ -16,16 +16,24 @@ const Main: React.FC = () => {
 
   useCurrentUserDetection();
 
-  switch (page) {
-    case HOME:
-      return <Calendar />;
-    case LOGIN:
-      return <LogIn />;
-    case REGISTER:
-      return <Register />;
-    case PROFILE:
-      return <Profile />;
-  }
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      {(() => {
+        switch (page) {
+          case HOME:
+            return <Calendar />;
+          case LOGIN:
+            return <LogIn />;
+          case REGISTER:
+            return <Register />;
+          case PROFILE:
+            return <Profile />;
+          default:
+            return null;
+        }
+      })()}
+    </React.Suspense>
+  );
 };
 
 export default Main;
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-redux", "redux"],
+          mui: ["@mui/material", "@mui/icons-material"],
+          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
+        },
+      },
+    },
+  },
 });
